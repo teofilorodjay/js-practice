ae6434ee46791723588be94fb7759a1a1abe81ff
teofilorodjay <teofilorodjay@gmail.com>
2024-09-23 09:10:40 +0800

    Update booleans.js

---
+++ booleans.js
@@ -0,0 +1,9 @@
+let isJsProgrammingLanguange = true;
+let isJsHard = true;
+let favNumb = 12;
+let result = favNumb + undefined;
+
+console.log(isJsProgrammingLanguange);
+console.log(isJsHard);
+console.log(favnumb);
+console.log(result);
